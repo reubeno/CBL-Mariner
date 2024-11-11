e3a5ad765d685192976b8050a88145ec663e7ae9
reuben olinsky <reubeno@users.noreply.github.com>
2024-11-11 10:38:43 -0800

    Updates

--- toolkit/scripts/run_ptests.py
+++ toolkit/scripts/run_ptests.py
@@ -11,6 +11,8 @@ import subprocess
 import sys
 import time
 
+from typing import Sequence
+
 logger = logging.getLogger(__name__)
 self_path = os.path.abspath(__file__)
 script_dir_path = os.path.dirname(self_path)
@@ -22,10 +24,10 @@ logs_dir_path = os.path.join(build_dir_path, "logs")
 ptest_logs_dir_path = os.path.join(logs_dir_path, "pkggen", "rpmbuilding")
 
 parser = argparse.ArgumentParser(description="Run package tests")
-parser.add_argument("-s", "--spec", dest="specs", action="append", help="Names of specs to run tests for")
+parser.add_argument("-s", "--spec", dest="specs", action="append", help="Names of base specs to run tests for")
+parser.add_argument("-e", "--extended-spec", dest="extended_specs", action="append", help="Names of extended specs to run tests for")
 parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Enable verbose output")
 parser.add_argument("--markdown-report", dest="markdown_report", help="Path to output markdown report of test results")
-parser.add_argument("--report-last-results-only", dest="report_last_results_only", action="store_true", help="Report only the last test results without re-running tests")
 
 args = parser.parse_args()
 
@@ -40,18 +42,8 @@ if not os.path.isfile(os.path.join(toolkit_dir_path, "Makefile")):
     logger.error("can't find toolkit path")
     sys.exit(1)
 
-#
-# Run.
-#
 
-if args.report_last_results_only:
-    logger.info("reporting last test results only; skipping build/test")
-
-else:
-    if not args.specs:
-        logger.error("no specs provided")
-        sys.exit(1)
-
+def run_ptests(spec_names: Sequence[str], specs_dir_path: str):
     toolkit_log_level = "info" if args.verbose else "warn"
 
     # N.B. We do *not* use TEST_RUN_LIST or TEST_RERUN_LIST, because those
@@ -64,9 +56,10 @@ else:
         "-j",
         str(multiprocessing.cpu_count()),
         "REBUILD_TOOLS=y",
+        f"SPECS_DIR={specs_dir_path}",
         f"LOG_LEVEL={toolkit_log_level}",
-        f"SRPM_PACK_LIST={' '.join(args.specs)}",
-        f"PACKAGE_REBUILD_LIST={' '.join(args.specs)}",
+        f"SRPM_PACK_LIST={' '.join(spec_names)}",
+        f"PACKAGE_REBUILD_LIST={' '.join(spec_names)}",
         "RUN_CHECK=y",
         "DAILY_BUILD_ID=lkg"
     ]
@@ -81,10 +74,6 @@ else:
     elapsed_time = time.time() - start_time
 
     logger.info(f"Ran tests in {elapsed_time:.2f}s.")
-
-#
-# Now analyze
-#
 
 class ReadableTestReporter:
     def __init__(self, display_log_paths: bool = True):
@@ -173,18 +162,67 @@ class MarkdownTestReporter:
         self._report_file.write(line)
         self._report_file.write("\n")
 
-logger.debug(f"Analyzing test results: {test_results_path}")
 
-with open(test_results_path, "r") as test_results:
-    test_results_text = test_results.read()
-    test_results = json.loads(test_results_text)
+def analyze_test_results():
+    logger.debug(f"Analyzing test results: {test_results_path}")
 
-unexpected_fail_count = 0
-expected_fail_count = 0
-skip_count = 0
-block_count = 0
-expected_success_count = 0
-unexpected_success_count = 0
+    with open(test_results_path, "r") as test_results:
+        test_results_text = test_results.read()
+        test_results = json.loads(test_results_text)
+
+    #
+    # Report results.
+    #
+
+    # TODO: Figure out which components didn't *have* any checks to run.
+    for srpm_name, srpm_results in test_results.items():
+        result = srpm_results["Result"]
+        expected_failure = srpm_results["ExpectedFailure"]
+
+        display_log_path = False
+        if result == "skipped":
+            for reporter in reporters:
+                reporter.on_skipped(srpm_name)
+            skip_count += 1
+        elif result == "blocked":
+            for reporter in reporters:
+                reporter.on_blocked(srpm_name)
+            block_count += 1
+        elif result == "failed":
+            if expected_failure:
+                expected_fail_count += 1
+            else:
+                unexpected_fail_count += 1  
+            for reporter in reporters:
+                reporter.on_failed(srpm_name, expected_failure, srpm_results["LogPath"])
+        elif result == "succeeded":
+            if expected_failure:
+                unexpected_success_count += 1
+            else:
+                expected_success_count += 1
+            for reporter in reporters:
+                reporter.on_succeeded(srpm_name, expected_failure)
+        else:
+            for reporter in reporters:
+                reporter.on_unknown_result(srpm_name)
+
+#
+# Run tests
+#
+
+if not args.specs and not args.extended_specs:
+    logger.error("no specs provided")
+    sys.exit(1)
+
+if args.specs:
+    run_ptests(args.specs, os.path.join(repo_root_dir_path, "SPECS"))
+
+if args.extended_specs:
+    run_ptests(args.extended_specs, os.path.join(repo_root_dir_path, "SPECS-EXTENDED"))
+
+#
+# Analyze results.
+#
 
 reporters = [ReadableTestReporter()]
 
@@ -193,44 +231,16 @@ if args.markdown_report:
     markdown_reporter = MarkdownTestReporter(args.markdown_report)
     reporters.append(markdown_reporter)
 
-#
-# Report results.
-#
-
-# TODO: Figure out which components didn't *have* any checks to run.
-for srpm_name, srpm_results in test_results.items():
-    result = srpm_results["Result"]
-    expected_failure = srpm_results["ExpectedFailure"]
-
-    display_log_path = False
-    if result == "skipped":
-        for reporter in reporters:
-            reporter.on_skipped(srpm_name)
-        skip_count += 1
-    elif result == "blocked":
-        for reporter in reporters:
-            reporter.on_blocked(srpm_name)
-        block_count += 1
-    elif result == "failed":
-        if expected_failure:
-            expected_fail_count += 1
-        else:
-            unexpected_fail_count += 1  
-        for reporter in reporters:
-            reporter.on_failed(srpm_name, expected_failure, srpm_results["LogPath"])
-    elif result == "succeeded":
-        if expected_failure:
-            unexpected_success_count += 1
-        else:
-            expected_success_count += 1
-        for reporter in reporters:
-            reporter.on_succeeded(srpm_name, expected_failure)
-    else:
-        for reporter in reporters:
-            reporter.on_unknown_result(srpm_name)
+unexpected_fail_count = 0
+expected_fail_count = 0
+skip_count = 0
+block_count = 0
+expected_success_count = 0
+unexpected_success_count = 0
 
 if markdown_reporter:
     markdown_reporter.close()
+
 
 #
 # Display a readable summary.
